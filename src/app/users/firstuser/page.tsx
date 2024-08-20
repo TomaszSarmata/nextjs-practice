33c11158364ebff157a0d14a81fe76079e88a39b
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-20 16:20:19 +0100

    created nested routing

---
+++ src/app/users/firstuser/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function page() {
+  return (
+    <div className="bg-yellow-800">
+      <h1>First User</h1>
+    </div>
+  );
+}
---
+++ src/app/users/seconduser/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SecondUser() {
+  return (
+    <div>
+      <h1 className="bg-red-100">Second User</h1>
+    </div>
+  );
+}
