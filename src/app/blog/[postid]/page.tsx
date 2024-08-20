01d399f45f3beb4015c50452b3f71f8d04f7cc88
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-20 16:39:25 +0100

    add type to params object

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function DetailsPage() {
+export default function DetailsPage({
+  params,
+}: {
+  params: { postid: string };
+}) {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1 className="text-9xl">Details Page with the id: {params.postid}</h1>
     </div>
   );
 }
