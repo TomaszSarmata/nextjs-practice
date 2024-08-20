39dc9c1cb6267124d0da154ce523ac50e31a8cfa
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-20 16:32:25 +0100

    add created routing for the blog details page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>Blog Post 1</h1>
+      <h1>Blog Post 2</h1>
+      <h1>Blog Post 3</h1>
+      <h1>Blog Post 4</h1>
+      <h1>Blog Post 5</h1>
+      <h1>Blog Post 6</h1>
+      <h1>Blog Post 7</h1>
+      <h1>Blog Post 8</h1>
+    </div>
+  );
+}
