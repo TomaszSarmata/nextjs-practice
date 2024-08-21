aa2964e0e95505f7f8fd1064f98a48f8773d874f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 10:09:00 +0100

    add route group layout to exclude one user from custom layout within the url segment

---
+++ src/app/users/thirduser/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ThirdUser() {
+  return (
+    <div className="bg-red-200 h-screen">
+      <h1 className="p-4 pt-20 text-8xl font-extralight">Third User</h1>
+    </div>
+  );
+}
