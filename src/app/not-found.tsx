695327342bc6dfb33c08e7dde1aa80bdb9054af8
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 08:21:26 +0100

    add custom-made  not found page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-5xl">Page Not Found</h1>
+      <h2 className="mt-3 text-2xl">
+        The Requested Resource Could Not be Found
+      </h2>
+      <p>Please make sure the url address is correct</p>
+    </div>
+  );
+}
