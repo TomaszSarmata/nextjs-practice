e0baa750c9ddd968f6d8b56989b2f9a5bc3410c3
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 09:57:37 +0100

    create custom layout for a group of route pages (user)

--- src/app/users/firstuser/page.tsx
+++ src/app/users/firstuser/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function page() {
   return (
-    <div className="bg-yellow-800">
-      <h1>First User</h1>
+    <div>
+      <h1 className="text-5xl">First User</h1>
     </div>
   );
 }
--- src/app/users/seconduser/page.tsx
+++ src/app/users/seconduser/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SecondUser() {
   return (
     <div>
-      <h1 className="bg-red-100">Second User</h1>
+      <h1 className="bg-blue-100">Second User</h1>
     </div>
   );
 }
