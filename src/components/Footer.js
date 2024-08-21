9ff22d8146441477b00647a3beac27e058222efb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-21 09:37:38 +0100

    code clean-up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="bg-yellow-300">
-      <h1 className="text-5xl font-serif">Footer Here</h1>
+      <h1 className="text-8xl font-serif">Footer Here</h1>
     </div>
   );
 }
