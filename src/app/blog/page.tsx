0d8904151e794205b944234d8c951a261b3b3e42
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-22 16:19:58 +0100

    implemented dynamic routing for detail blog post pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,14 +12,30 @@ export default function BlogPage() {
   return (
     <div>
       <Link href="/">Home</Link>
-      <h1>Blog Post 1</h1>
-      <h1>Blog Post 2</h1>
-      <h1>Blog Post 3</h1>
-      <h1>Blog Post 4</h1>
-      <h1>Blog Post 5</h1>
-      <h1>Blog Post 6</h1>
-      <h1>Blog Post 7</h1>
-      <h1>Blog Post 8</h1>
+      <h1>
+        <Link href="/blog/1">Blog Post 1</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/2">Blog Post 2</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/3">Blog Post 3</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/4">Blog Post 4</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/5">Blog Post 5</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/6">Blog Post 6</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/7">Blog Post 7</Link>
+      </h1>
+      <h1>
+        <Link href="/blog/8">Blog Post 8</Link>
+      </h1>
     </div>
   );
 }
