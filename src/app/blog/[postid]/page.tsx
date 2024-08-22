69bfb38072911f5d43951374de881e636357cf9d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-22 15:07:14 +0100

    generate dynamic metadata for each blog post

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Metadata } from 'next';
 
-export default function DetailsPage({
-  params,
-}: {
-  params: { postid: string };
-}) {
+type Props = {
+  params: {
+    postid: string;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Blog Post ${params.postid}`,
+  };
+};
+
+export default function DetailsPage({ params }: Props) {
+  console.log(params, 'here params');
   return (
     <div>
       <h1 className="text-9xl">Details Page with the id: {params.postid}</h1>
